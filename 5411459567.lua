9533a2414da427872f5d1048d6e1a1ec1ed6ffc2
Rishy <139991713+rishyrawr@users.noreply.github.com>
2023-09-04 02:22:32 +0700

    Update 5411459567.lua

--- 5411459567.lua
+++ 5411459567.lua
@@ -4,17 +4,8 @@ if game.PlaceId == 5411459567 then
     repeat task.wait(1) until game:GetService("Workspace").Message
 end
 
-local function AutoLoad()
-    if syn then
-        syn.queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/rishyrawr/Main/PlaceId/5411459567.lua", true))()')
-    else
-        queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/rishyrawr/Main/PlaceId/5411459567.lua", true))()')
-    end
-end
-
 if game.GameId == 578392296 then
     if game.PlaceId ~= 5411459567 then
-        AutoLoad()
         game:GetService('TeleportService'):Teleport(5411459567)
     end
 
@@ -26,11 +17,9 @@ if game.GameId == 578392296 then
 
     if timerCheck() <= 60 then
     repeat task.wait() until timerCheck() > 60
-        task.wait(5)
-        AutoLoad()
+        task.wait(1)
         loadstring(game:HttpGet("https://raw.githubusercontent.com/rishyrawr/Main/PlaceId/Laiteux.lua", true))()
     else
-        AutoLoad()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/rishyrawr/Main/PlaceId/Laiteux.lua", true))()
     end
 end
